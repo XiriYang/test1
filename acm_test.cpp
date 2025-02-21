2d1249b945636b01ccaa8bd1e7c47c4c74a2821c
XiriYang <202400130027@mail.sdu.edu.cn>
2025-02-21 23:15:03 +0800

    add acm_test

---
+++ acm_test.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+const float PI = 3.141593;
+const float FENCE_PRICE = 35;
+const float CONCRETE_PRICE = 20;
+class Circle {
+public:
+	Circle(float r);
+	float circumference();
+	float area();
+	~Circle() {};
+private:
+	float radius;
+};
+int main()
+{
+	float radius;
+	cout << "Enter the radius of the pool: ";
+	cin >> radius;
+	Circle pool(radius);
+	Circle poolrim(radius+3);
+	auto fencecost = poolrim.circumference() * FENCE_PRICE;
+	auto concretecost = (poolrim.area() - pool.area()) * CONCRETE_PRICE;
+	cout << fencecost << endl<< concretecost << endl;
+	return 0;
+}
+Circle::Circle(float r)
+{
+	radius = r;
+}
+float Circle::circumference()
+{
+	return 2 * PI * radius;
+}
+float Circle::area()
+{
+	return PI * radius * radius;
+}
